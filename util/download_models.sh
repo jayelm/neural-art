aca303fc106149173bbe031ddf2352056dfb83e1
Jesse Mu <muj@bc.edu>
2015-10-28 22:01:00 -0400

    Make models dir in download models script

--- util/download_models.sh
+++ util/download_models.sh
@@ -1,3 +1,4 @@
+mkdir ../models
 cd ../models
 wget -c https://gist.githubusercontent.com/ksimonyan/3785162f95cd2d5fee77/raw/bb2b4fe0a9bb0669211cf3d0bc949dfdda173e9e/VGG_ILSVRC_19_layers_deploy.prototxt
 wget -c --no-check-certificate https://bethgelab.org/media/uploads/deeptextures/vgg_normalised.caffemodel
